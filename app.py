b26e7b1138cac7d620502d6b6f553a73830686a8
TommyPadron <101023760+TommyPadron@users.noreply.github.com>
2026-08-11 15:04:09 -0400

    boton de editar pedido

--- app.py
+++ app.py
@@ -1026,14 +1026,34 @@ if pagina == "Consultas":
     st.write(
         f"Pedido seleccionado: #{pedido_id:04d}"
     )
+
+
     if st.button(
         "✏️ Editar Pedido"
     ):
 
+        pedido = obtener_orden_por_id(
+            pedido_id
+        )
+
         st.success(
             f"Editando Pedido #{pedido_id:04d}"
-        )    
+        )
 
+        nombre_edit = st.text_input(
+            "Nombre",
+            value=pedido["nombre"]
+        )
+
+        telefono_edit = st.text_input(
+            "Teléfono",
+            value=pedido["telefono"]
+        )
+
+        correo_edit = st.text_input(
+            "Correo",
+            value=pedido["correo"]
+        )
     if st.button(
         "📊 Exportar Excel"
     ):
